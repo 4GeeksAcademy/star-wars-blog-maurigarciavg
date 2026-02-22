c65b7777e67b753813400da82972f1f5d4e4e88d
Mauri García <63955780+maurigarciavg@users.noreply.github.com>
2026-02-22 13:12:18 +0000

    tarjetas y estilos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,25 @@
 import { Link } from "react-router-dom";
+import starWarsLogo from "/src/assets/img/star-wars-logo.png";
 
 export const Navbar = () => {
 
 	return (
-		<nav className="navbar navbar-light bg-light">
+		<nav className="navbar ">
 			<div className="container">
 				<Link to="/">
-					<h1 className="navbar-brand mb-0 h1">STAR WARS</h1>
+					<img src={starWarsLogo} alt="Star Wars Logo" className="main-logo" />
 				</Link>
 				<div className="ml-auto">
 					<Link to="/demo">
-						<span className="btn btn-primary">HOME</span>
-						<span className="btn btn-primary">CHARACTERS</span>
-						<span className="btn btn-primary">PLANETS</span>
+						<span className="btn btn-primary">Home</span>
+						<span className="btn btn-primary">Characters</span>
+						<span className="btn btn-primary">Planets</span>
 						<span className="btn btn-primary">STARSHIPS</span>
-						<div class="dropdown">
-							<button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-								FAUVORITES
+						<button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+								Favorites
 							</button>
+						<div class="dropdown">
+							
 							<ul class="dropdown-menu">
 								<li><a class="dropdown-item" href="#">Action</a></li>
 								<li><a class="dropdown-item" href="#">Another action</a></li>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,89 +1,48 @@
-import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+
 
 export const Home = () => {
 
 	const { store, dispatch } = useGlobalReducer()
 
 	return (
-		<div className="container text-center mt-5">
+		<div className="container mt-5">
 			<div className="container text-center mt-5">
-				<div className="row">
-					<div className="card col" style={{ width: "18rem" }}>
-						<img src="https://raw.githubusercontent.com/tbone849/star-wars-guide/master/build/assets/img/characters/1.jpg" className="card-img-top" alt="Luke Skywalker" />
-						<div className="card-body">
-							<h5 className="card-title">Luke Skywalker</h5>
-							<p className="card-text">Un gran caballero Jedi de Tatooine.</p>
-							<a href="#" className="btn btn-primary">Ver detalles</a>
-						</div>
-					</div>
-					<div className="card col" style={{ width: "18rem" }}>
-						<img src="https://raw.githubusercontent.com/tbone849/star-wars-guide/master/build/assets/img/characters/1.jpg" className="card-img-top" alt="Luke Skywalker" />
-						<div className="card-body">
-							<h5 className="card-title">Luke Skywalker</h5>
-							<p className="card-text">Un gran caballero Jedi de Tatooine.</p>
-							<a href="#" className="btn btn-primary">Ver detalles</a>
-						</div>
-					</div>
-					<div className="card col" style={{ width: "18rem" }}>
-						<img src="https://raw.githubusercontent.com/tbone849/star-wars-guide/master/build/assets/img/characters/1.jpg" className="card-img-top" alt="Luke Skywalker" />
-						<div className="card-body">
-							<h5 className="card-title">Luke Skywalker</h5>
-							<p className="card-text">Un gran caballero Jedi de Tatooine.</p>
-							<a href="#" className="btn btn-primary">Ver detalles</a>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div>
-				<div className="container text-center mt-5">
-					<div className="row">
-						<div className="card col" style={{ width: "18rem" }}>
-							<img
-								src="https://raw.githubusercontent.com/tbone849/star-wars-guide/refs/heads/master/build/assets/img/planets/2.jpg"
-								className="card-img-top"
-								alt="Tatooine"
-							/>
-							<div className="card-body">
-								<h5 className="card-title">Alderaan</h5>
-								<p className="card-text">
-									Conocido como la "Joya de los Mundos del Núcleo", Alderaan era un planeta de belleza inigualable, famoso por su cultura pacífica, sus montañas nevadas y por ser el hogar de la princesa Leia Organa.
-								</p>
-								<a href="#" className="btn btn-primary">
-									Ver detalles del planeta
-								</a>
+				<h1 className="text-center sw-title mb-5">Characters</h1>
+				<div className="row g-4">
+					<div className="col-12 col-md-4">
+						<div className="card h-100 bg-dark border-secondary">
+							<div className="img-container">
+								<img src="https://raw.githubusercontent.com/tbone849/star-wars-guide/master/build/assets/img/characters/1.jpg" alt="Luke" />
+							</div>
+							<div className="card-body d-flex flex-column text-center">
+								<h5 className="card-title sw-title text-warning">Luke Skywalker</h5>
+								<p className="card-text sw-text text-limit">Un gran caballero Jedi de Tatooine capacitado en las artes de la Fuerza.</p>
+								<button className="btn btn-outline-warning mt-auto">Ver detalles</button>
 							</div>
 						</div>
-						<div className="card col" style={{ width: "18rem" }}>
-							<img
-								src="https://raw.githubusercontent.com/tbone849/star-wars-guide/refs/heads/master/build/assets/img/planets/2.jpg"
-								className="card-img-top"
-								alt="Tatooine"
-							/>
-							<div className="card-body">
-								<h5 className="card-title">Alderaan</h5>
-								<p className="card-text">
-									Conocido como la "Joya de los Mundos del Núcleo", Alderaan era un planeta de belleza inigualable, famoso por su cultura pacífica, sus montañas nevadas y por ser el hogar de la princesa Leia Organa.
-								</p>
-								<a href="#" className="btn btn-primary">
-									Ver detalles del planeta
-								</a>
+					</div>
+					<div className="col-12 col-md-4">
+						<div className="card h-100 bg-dark border-secondary">
+							<div className="img-container">
+								<img src="https://raw.githubusercontent.com/tbone849/star-wars-guide/master/build/assets/img/characters/1.jpg" alt="Luke" />
+							</div>
+							<div className="card-body d-flex flex-column text-center">
+								<h5 className="card-title sw-title text-warning">Luke Skywalker</h5>
+								<p className="card-text sw-text text-limit">Un gran caballero Jedi de Tatooine capacitado en las artes de la Fuerza.</p>
+								<button className="btn btn-outline-warning mt-auto">Ver detalles</button>
 							</div>
 						</div>
-						<div className="card col" style={{ width: "18rem" }}>
-							<img
-								src="https://raw.githubusercontent.com/tbone849/star-wars-guide/refs/heads/master/build/assets/img/planets/2.jpg"
-								className="card-img-top"
-								alt="Tatooine"
-							/>
-							<div className="card-body">
-								<h5 className="card-title">Alderaan</h5>
-								<p className="card-text">
-									Conocido como la "Joya de los Mundos del Núcleo", Alderaan era un planeta de belleza inigualable, famoso por su cultura pacífica, sus montañas nevadas y por ser el hogar de la princesa Leia Organa.
-								</p>
-								<a href="#" className="btn btn-primary">
-									Ver detalles del planeta
-								</a>
+					</div>
+					<div className="col-12 col-md-4">
+						<div className="card h-100 bg-dark border-secondary">
+							<div className="img-container">
+								<img src="https://raw.githubusercontent.com/tbone849/star-wars-guide/master/build/assets/img/characters/1.jpg" alt="Luke" />
+							</div>
+							<div className="card-body d-flex flex-column text-center">
+								<h5 className="card-title sw-title text-warning">Luke Skywalker</h5>
+								<p className="card-text sw-text text-limit">Un gran caballero Jedi de Tatooine capacitado en las artes de la Fuerza.</p>
+								<button className="btn btn-outline-warning mt-auto">Ver detalles</button>
 							</div>
 						</div>
 					</div>
@@ -91,53 +50,85 @@ export const Home = () => {
 			</div>
 			<div>
 				<div className="container text-center mt-5">
-					<div className="row">
-						<div className="card col" style={{ width: "18rem" }}>
-							<img
-								src="https://raw.githubusercontent.com/tbone849/star-wars-guide/refs/heads/master/build/assets/img/starships/10.jpg"
-								className="card-img-top"
-								alt="Halcón Milenario"
-							/>
-							<div className="card-body">
-								<h5 className="card-title">Halcón Milenario</h5>
-								<p className="card-text">
-									La nave más rápida de la galaxia, capaz de hacer la Carrera Kessel en menos de 12 pársecs. El orgullo de Han Solo y Chewbacca.
-								</p>
-								<a href="#" className="btn btn-primary">
-									Detalles de la nave
-								</a>
+					<h1 className="text-center sw-title mb-5">Planets</h1>
+					<div className="row g-4">
+						<div className="col-12 col-md-4">
+							<div className="card h-100 bg-dark border-secondary">
+								<div className="img-container">
+									<img src="https://raw.githubusercontent.com/tbone849/star-wars-guide/refs/heads/master/build/assets/img/planets/2.jpg" alt="Alderaan" />
+								</div>
+								<div className="card-body d-flex flex-column text-center">
+									<h5 className="card-title sw-title text-warning">Alderaan</h5>
+									<p className="card-text sw-text text-limit">Conocido como la "Joya de los Mundos del Núcleo", Alderaan era un planeta de belleza inigualable, famoso por su cultura pacífica.</p>
+									<button className="btn btn-outline-warning mt-auto">Ver detalles</button>
+								</div>
 							</div>
 						</div>
-						<div className="card col" style={{ width: "18rem" }}>
-							<img
-								src="https://raw.githubusercontent.com/tbone849/star-wars-guide/refs/heads/master/build/assets/img/starships/10.jpg"
-								className="card-img-top"
-								alt="Halcón Milenario"
-							/>
-							<div className="card-body">
-								<h5 className="card-title">Halcón Milenario</h5>
-								<p className="card-text">
-									La nave más rápida de la galaxia, capaz de hacer la Carrera Kessel en menos de 12 pársecs. El orgullo de Han Solo y Chewbacca.
-								</p>
-								<a href="#" className="btn btn-primary">
-									Detalles de la nave
-								</a>
+						<div className="col-12 col-md-4">
+							<div className="card h-100 bg-dark border-secondary">
+								<div className="img-container">
+									<img src="https://raw.githubusercontent.com/tbone849/star-wars-guide/refs/heads/master/build/assets/img/planets/2.jpg" alt="Alderaan" />
+								</div>
+								<div className="card-body d-flex flex-column text-center">
+									<h5 className="card-title sw-title text-warning">Alderaan</h5>
+									<p className="card-text sw-text text-limit">Conocido como la "Joya de los Mundos del Núcleo", Alderaan era un planeta de belleza inigualable, famoso por su cultura pacífica.</p>
+									<button className="btn btn-outline-warning mt-auto">Ver detalles</button>
+								</div>
 							</div>
 						</div>
-						<div className="card col" style={{ width: "18rem" }}>
-							<img
-								src="https://raw.githubusercontent.com/tbone849/star-wars-guide/refs/heads/master/build/assets/img/starships/10.jpg"
-								className="card-img-top"
-								alt="Halcón Milenario"
-							/>
-							<div className="card-body">
-								<h5 className="card-title">Halcón Milenario</h5>
-								<p className="card-text">
-									La nave más rápida de la galaxia, capaz de hacer la Carrera Kessel en menos de 12 pársecs. El orgullo de Han Solo y Chewbacca.
-								</p>
-								<a href="#" className="btn btn-primary">
-									Detalles de la nave
-								</a>
+						<div className="col-12 col-md-4">
+							<div className="card h-100 bg-dark border-secondary">
+								<div className="img-container">
+									<img src="https://raw.githubusercontent.com/tbone849/star-wars-guide/refs/heads/master/build/assets/img/planets/2.jpg" alt="Alderaan" />
+								</div>
+								<div className="card-body d-flex flex-column text-center">
+									<h5 className="card-title sw-title text-warning">Alderaan</h5>
+									<p className="card-text sw-text text-limit">Conocido como la "Joya de los Mundos del Núcleo", Alderaan era un planeta de belleza inigualable, famoso por su cultura pacífica.</p>
+									<button className="btn btn-outline-warning mt-auto">Ver detalles</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div>
+				<div className="container text-center mt-5">
+					<h1 className="text-center sw-title mb-5">Starships</h1>
+					<div className="row g-4">
+						<div className="col-12 col-md-4">
+							<div className="card h-100 bg-dark border-secondary">
+								<div className="img-container">
+									<img src="https://raw.githubusercontent.com/tbone849/star-wars-guide/refs/heads/master/build/assets/img/starships/10.jpg" alt="Falcon" />
+								</div>
+								<div className="card-body d-flex flex-column text-center">
+									<h5 className="card-title sw-title text-warning">Halcon Milenario</h5>
+									<p className="card-text sw-text text-limit">La nave más rápida de la galaxia, capaz de hacer la Carrera Kessel en menos de 12 pársecs.</p>
+									<button className="btn btn-outline-warning mt-auto">Ver detalles</button>
+								</div>
+							</div>
+						</div>
+						<div className="col-12 col-md-4">
+							<div className="card h-100 bg-dark border-secondary">
+								<div className="img-container">
+									<img src="https://raw.githubusercontent.com/tbone849/star-wars-guide/refs/heads/master/build/assets/img/starships/10.jpg" alt="Falcon" />
+								</div>
+								<div className="card-body d-flex flex-column text-center">
+									<h5 className="card-title sw-title text-warning">Halcon Milenario</h5>
+									<p className="card-text sw-text text-limit">La nave más rápida de la galaxia, capaz de hacer la Carrera Kessel en menos de 12 pársecs.</p>
+									<button className="btn btn-outline-warning mt-auto">Ver detalles</button>
+								</div>
+							</div>
+						</div>
+						<div className="col-12 col-md-4">
+							<div className="card h-100 bg-dark border-secondary">
+								<div className="img-container">
+									<img src="https://raw.githubusercontent.com/tbone849/star-wars-guide/refs/heads/master/build/assets/img/starships/10.jpg" alt="Falcon" />
+								</div>
+								<div className="card-body d-flex flex-column text-center">
+									<h5 className="card-title sw-title text-warning">Halcon Milenario</h5>
+									<p className="card-text sw-text text-limit">La nave más rápida de la galaxia, capaz de hacer la Carrera Kessel en menos de 12 pársecs.</p>
+									<button className="btn btn-outline-warning mt-auto">Ver detalles</button>
+								</div>
 							</div>
 						</div>
 					</div>
